aa9945f25a9a44b28ff33e2af9ef46142e05b8ab
Dustin Hollis <dustin.hollis@live.com.au>
2016-04-15 18:38:56 +1000

    Formatting and Main Page added

--- client/index.js
+++ client/index.js
@@ -1,27 +1,11 @@
 var myApp = angular.module('myApp',[])
 
-myApp.service('ChangePage', function($http)) {
-	var baseUrl = "http://localhost:8080/"
-	this.saveWord = function (danceSelect) {
-		var url = baseUrl + danceSelect
-		return $http.post(url, {"dance": danceSelect})
-	}
-}
-
 myApp.controller('MyController', function($scope) {
 
 	$scope.danceSelect = waltz
-
-	$scope.beginDance = function(){
-		ChangePage.saveWord($scope.danceSelect)
-		.then(saveSuccess, error)
-	}
-
-	function saveSuccess(json) {
-		console.log(json)
-	}
 
 	function error(err) {
 		console.log(err)
 	}
 })
+//This is a comment
