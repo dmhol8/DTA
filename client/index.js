365d8387634a67f0977d78850d09283811ca405b
Dustin Hollis <dustin.hollis@live.com.au>
2016-04-25 22:03:05 +1000

    Create figure form layout created

--- client/index.js
+++ client/index.js
@@ -1,6 +1,15 @@
 var myApp = angular.module('myApp',[])
 src="jquery-1.12.3.js"
 
+function numberList(steps) {
+
+	var i, text;
+	for (i = 1; i < steps+1; i++){
+		text += "<option>" + i + "</option>";
+	}
+	return text;
+}
+
 myApp.controller('MyController', function($scope) {
 
 	$scope.danceSelect = waltz
@@ -10,8 +19,6 @@ myApp.controller('MyController', function($scope) {
 	}
 })
 
- $(document).ready(function(){
-
-   // jQuery methods go here...
-
-});
+document.getElementById("selectStepNo").innerHTML = numberList(5);
+document.getElementById("selectPreNo").innerHTML = numberList(5);
+document.getElementById("selectFollNo").innerHTML = numberList(5);
