5ad7ff81c7eeac09255ab476f76d78f746117c91
Dustin Hollis <dustin.hollis@live.com.au>
2016-04-29 15:26:54 +1000

    Merged pages into one and worked on display functions

--- client/index.js
+++ client/index.js
@@ -1,24 +1,63 @@
 var myApp = angular.module('myApp',[])
 src="jquery-1.12.3.js"
 
-function numberList(steps) {
+myApp.service('CreateDropdown', function() {
 
-	var i, text;
-	for (i = 1; i < steps+1; i++){
-		text += "<option>" + i + "</option>";
+	this.numberList = function numberList(steps) {
+		var i, text;
+		for (i = 1; i < steps+1; i++){
+			text += "<option>" + i + "</option>";
+		}
+		return text;
 	}
-	return text;
-}
+});
 
-myApp.controller('MyController', function($scope) {
+myApp.service('VisibilityService', function() {
 
-	$scope.danceSelect = waltz
+	this.visibleChange = function visibleChange() {
+		document.getElementById("introPage").style.display = "none";
+		document.getElementById("mainPage").style.display = "block";
+	}
+
+});
+
+myApp.controller('myController', function($scope) {
+
+	$scope.danceSelect = waltz;
 
 	function error(err) {
-		console.log(err)
-	}
-})
+		console.log(err);
+	};
+});
 
 document.getElementById("selectStepNo").innerHTML = numberList(5);
 document.getElementById("selectPreNo").innerHTML = numberList(5);
 document.getElementById("selectFollNo").innerHTML = numberList(5);
+
+function visMain() {
+    document.getElementById("introPage").style.display = "none";
+    document.getElementById("mainPage").style.display = "block";
+}
+
+function visIndex() {
+	document.getElementById("mainPage").style.display = "none";
+    document.getElementById("introPage").style.display = "block";
+}
+
+function visFig() {
+	document.getElementById("options").style.display = "none";
+    document.getElementById("section4CreateFigure").style.display = "inline-block";
+}
+
+function visOpt() {
+	document.getElementById("section4CreateFigure").style.display = "none";
+	document.getElementById("options").style.display = "inline-block";
+}
+
+function visCreateFig() {
+	document.getElementById("figDetailBox").style.display = "block";
+}
+
+function visNoCreateFig() {
+	document.getElementById("figDetailBox").style.display = "none";
+}
