ce419c57e61f564480a03558ae1be8233ecd9015
Dustin Hollis <dustin.hollis@live.com.au>
2016-05-03 22:24:46 +1000

    Started creating database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var mongoDB = require('mongodb')
 var done = function(){
 	console.log('I just wrote to the database')
 	res.end("done")
