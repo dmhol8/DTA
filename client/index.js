bb4d3dcf8c2773fa8d038f27df10a096bb80373e
Dustin Hollis <dustin.hollis@live.com.au>
2016-09-05 22:13:04 +1000

    Added logo and print option

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp',[])
 
-src="node_modules/angular/jqlite.js"
+
 
 myApp.directive("anotherfig", function($compile){
 	return function(scope, element, attrs){
@@ -641,6 +641,51 @@ myApp.controller('myController', function($scope, NumberService, VisibilityServi
   	$scope.noDetail = function () {
   		document.getElementById("detailPage").style.display = "none";
   		document.getElementById("mainPage").style.display = "block";
+  	}
+
+  	$scope.printPdf = function () {
+
+  		window.print();
+		// var doc = new jsPDF('landscape', 'pt', 'a4');
+
+        // // source can be HTML-formatted string, or a reference
+        // // to an actual DOM element from which the text will be scraped.
+        // source = angular.element(document.querySelector("#detailPage")).html();
+
+
+        // // we support special element handlers. Register them with jQuery-style 
+        // // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+        // // There is no support for any other type of selectors 
+        // // (class, of compound) at this time.
+        // specialElementHandlers = {
+        //     // element with id of "bypass" - jQuery style selector
+        //     '#bypassme': function (element, renderer) {
+        //         // true = "handled elsewhere, bypass text extraction"
+        //         return true
+        //     }
+        // };
+        // margins = {
+        //     top: 40,
+        //     bottom: 40,
+        //     left: 40,
+        //     width: 580
+        // };
+        // // all coords and widths are in jsPDF instance's declared units
+        // // 'inches' in this case
+        // pdf.fromHTML(
+        // source, // HTML string or DOM elem ref.
+        // margins.left, // x coord
+        // margins.top, { // y coord
+        //     'width': margins.width, // max width of content on PDF
+        //     'elementHandlers': specialElementHandlers
+        // },
+
+        // function (dispose) {
+        //     // dispose: object with X, Y of the last line add to the PDF 
+        //     //          this allow the insertion of new lines after html
+        //     pdf.save('Test.pdf');
+        // }, margins);
+
   	}
 
   	function error (err) {
